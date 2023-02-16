53e1e4acaf2ee78523ea79a333498e2781ce483a
Vijayant Katyal <1795103+vijayantkatyal@users.noreply.github.com>
2023-02-16 15:06:27 +0000

    [Added] letter case

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -73,6 +73,8 @@ import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
 import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
 import EditorWatchdog from '@ckeditor/ckeditor5-watchdog/src/editorwatchdog.js';
 
+import LetterCase from 'ckeditor5-letter-case';
+
 class Editor extends ClassicEditor {}
 
 // Plugins to include in the build.
@@ -144,7 +146,8 @@ Editor.builtinPlugins = [
 	TextTransformation,
 	TodoList,
 	Underline,
-	WordCount
+	WordCount,
+	LetterCase
 ];
 
 // Editor configuration.
@@ -175,6 +178,7 @@ Editor.defaultConfig = {
 			'alignment',
 			'subscript',
 			'superscript',
+			'LetterCase',
 			'-',
 			'outdent',
 			'indent',
